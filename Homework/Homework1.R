155fd4a0d8e16b05a87f8442f0cb149a7d0d511b
gaetanoDJ <77935085+gaetanoDJ@users.noreply.github.com>
2021-02-07 10:56:32 -0600

    Update Homework1.R

--- Homework/Homework1.R
+++ Homework/Homework1.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(ggplot2)
 
+##### A)
 d1 = GasPrices %>%
   group_by(Competitors) %>%
   summarize(Price)
@@ -9,10 +10,12 @@ d1
 ggplot(data = d1) +
   geom_boxplot(mapping = aes(x=Competitors, y=Price))
 
+# B)
 ggplot(data = GasPrices)+
   geom_point(mapping = aes(x = Income, y = Price, color = Brand)) +
   scale_x_continuous(breaks=seq(0,130000, 10000))
 
+# C)
 Price_Brand = GasPrices %>%
   group_by(Brand) %>%
   summarize(mean_price=mean(Price))
@@ -21,3 +24,12 @@ Price_Brand
 test <- ggplot(data = Price_Brand, aes(x=Brand, y=mean_price, fill=Brand))+
   geom_bar(stat = "identity")
 test
+
+######### D) 
+
+test1 <- ggplot(data = GasPrices)+
+  geom_histogram(aes(x=Price), bins = 30) +
+  facet_wrap(~ Stoplight)
+test1
+
+######### E) 
